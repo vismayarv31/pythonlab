05ef92f28c05d97e112491714fdcc3f32c62e919
vismayarv31 <149702061+vismayarv31@users.noreply.github.com>
2024-01-26 17:38:20 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,5 +1,27 @@
 print("using temporary variable:")
-a=5
-b=10
+a = 5
+b = 10
 print("a befor swapping:",a)
 print("a befor swapping:",a)
+temp = a
+a = b
+b = temp
+print("a after swapping:",a)
+print("b after swapping:",b)
+print("Without using temporary variable")
+a = 5
+b = 10
+print("a before swapping:",a)
+print("b before swapping:",b)
+a = a + b
+b = a - b
+a = a - b
+print("a after swapping:",a)
+print("b after swapping:",b)
+a = 5
+b = 10
+print("a before swapping:",a)
+print("b before swapping:",b)
+a,b = b,a
+print("a after swapping:",a)
+print("b after swapping:",b)
